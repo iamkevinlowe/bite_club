9fda1bd0c1de8a242b912b0f92b6200e218c01b7
Kevin Lowe <iamkevinlowe@gmail.com>
2016-02-27 11:06:57 -0800

    Fixed scrollPosition directive typo.

--- app/assets/javascripts/angular/directives/scrollPosition.directive.js
+++ app/assets/javascripts/angular/directives/scrollPosition.directive.js
@@ -4,19 +4,19 @@
     .directive('scrollPosition', ['$window', scrollPosition]);
 
   function scrollPosition($window) {
-      return {
-        link: function(scope, element, attrs) {
-          var windowEl = angular.element($window);
-          var handler = function() {
-            if (location.pathname == '/' && windowEl.scrollTop() < 50) {
-              element.css('opacity', 0);
-            } else {
-              element.css('opacity', 1);
-            }
-          };
-          windowEl.on('scroll', handler);
-          handler();
-        }
+    return {
+      link: function(scope, element, attrs) {
+        var windowEl = angular.element($window);
+        var handler = function() {
+          if (location.pathname == '/' && windowEl.scrollTop() < 50) {
+            element.css('opacity', 0);
+          } else {
+            element.css('opacity', 1);
+          }
+        };
+        windowEl.on('scroll', handler);
+        handler();
       }
-    })
+    }
+  }
 })();
