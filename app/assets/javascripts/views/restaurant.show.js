b7be94056984e276a2b63efde302750cb2bceec9
Kevin Lowe <iamkevinlowe@gmail.com>
2016-03-04 08:52:36 -0800

    Separated instagram carousel from main carousel.

--- app/assets/javascripts/views/restaurant.show.js
+++ app/assets/javascripts/views/restaurant.show.js
@@ -1,12 +1,40 @@
 $(document).on('page:change', function() {
   if (!document.getElementsByClassName('restaurant show')[0]) return;
 
-  $('.carousel-main, .carousel-instagram').owlCarousel({
+  var mainCarousel = $('.carousel-main');
+  var instagramCarousel = $('.carousel-instagram');
+
+  mainCarousel.owlCarousel({
     loop: true,
     autoplay: true,
     center: true,
     items: 1
   });
+
+  instagramCarousel.owlCarousel({
+    loop: true,
+    autoplay: true,
+    center: true,
+    items: 1
+  });
+
+  var instagramElements = document.getElementsByClassName('instagram-image');
+
+  for (var i = 0; i < instagramElements.length; i++) {
+    instagramElements[i].addEventListener('click', function(event) {
+      // instagramCarousel.trigger('stop.owl.autoplay');
+
+      // if (event.currentTarget.requestFullscreen) {
+      //   event.currentTarget.requestFullscreen();
+      // } else if (event.currentTarget.msRequestFullscreen) {
+      //   event.currentTarget.msRequestFullscreen();
+      // } else if (event.currentTarget.mozRequestFullScreen) {
+      //   event.currentTarget.mozRequestFullScreen();
+      // } else if (event.currentTarget.webkitRequestFullscreen) {
+      //   event.currentTarget.webkitRequestFullscreen();
+      // }
+    });
+  }
 
   $.ajax({
     method: 'GET',
